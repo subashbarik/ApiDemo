d72b8ac656140485e356f7a450dd0300f31415d2
SubashAsusX515 <subash.barik@gmail.com>
2023-01-26 11:47:30 +0530

    Added sample test project.

---
+++ ApiDemoUnitTests/APiDemoTests.cs
@@ -0,0 +1,31 @@
+namespace ApiDemoUnitTests
+{
+    public class APiDemoTests
+    {
+        [Fact]
+        public void APiDemo_Test1()
+        {
+            //Arrange
+            var actual = true; 
+            var expected = true;
+
+            //Act
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void APiDemo_Test2()
+        {
+            //Arrange
+            var actual = true;
+            var expected = true;
+
+            //Act
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
