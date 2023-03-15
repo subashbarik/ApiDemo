7149684aed3d4f415f50d1dcd5f46c94e51bfa40
SubashAsusX515 <subash.barik@gmail.com>
2023-03-15 11:55:30 +0530

    Added metod calculator and its tests

---
+++ ApiDemoUnitTests/CalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Business;
+
+namespace ApiDemoUnitTests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void CalculatorTests_add_should_pass()
+        {
+            //Arrange
+            int i = 2; int j = 3; int k = 5;
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.add(i, j);
+
+            //Assert
+            Assert.Equal(k, result);
+
+        }
+    }
+}
---
+++ WebAPI/Business/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPI.Business
+{
+    public class Calculator
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+        public int subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
